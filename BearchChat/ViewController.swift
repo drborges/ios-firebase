bcafc5440d7a8f5a53552d5f0330d782f9d82b79
Diego Borges <drborges.cic@gmail.com>
2015-07-13 19:06:38 -0300

    Spike on firebase SDK

--- BearchChat/ViewController.swift
+++ BearchChat/ViewController.swift
@@ -8,10 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var messages = [String]()
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.messages.append("Well hello there!")
+        tableView.dataSource = self
+        tableView.reloadData()
+        var ref = Firebase(url:"https://bearch-chat.firebaseio.com/messages")
+        ref.observeEventType(.ChildAdded, withBlock: { snapshot in
+            let message = snapshot.value
+            self.messages.append(message as! String)
+            self.tableView.reloadData()
+        })
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +34,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellId: NSString = "messagesCell"
+        var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellId as String) as! UITableViewCell
+        
+        let message = self.messages[indexPath.row]
+        cell.textLabel!.text = message
+        
+        return cell
+    }
 }
 
